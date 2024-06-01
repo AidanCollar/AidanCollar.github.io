f1bf974d196f27cfcfbd5eb1a76c2ef9f84b768f
AidanCollar <300088221@fvtc.edu>
2024-06-01 14:08:48 -0500

    added the homepage

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.static('public'))
+// app.use('/api', require('./routes/api-v1'))
+app.use('/',require('./routes/static'))
+
+app.listen(port, () => console.log(`Server is running http://localhost:${port}`))
